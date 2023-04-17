cf982b45d431bc7d764b77ea2d154070fd50bde2
MilesIlac <smile.caliprf97@gmail.com>
2023-04-17 23:21:28 +0800

    added Application class for Hilt

---
+++ app/src/main/java/com/milesilac/citylifescan/MyApplication.java
@@ -0,0 +1,9 @@
+package com.milesilac.citylifescan;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MyApplication extends Application {
+}
